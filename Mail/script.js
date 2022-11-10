3ffdf03c387ecdc4e6013c44f5ebfce7e4655a51
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-10 15:14:27 +0100

    Completamento eserciziuo email

---
+++ Mail/script.js
@@ -0,0 +1,23 @@
+let listEmail = [
+    'mariorossi@gmail.com',
+    'federicagialli@libero.it',
+    'stefanoverdi@hotmail.com'
+];
+let insertName = document.getElementById('email');
+let userVerified = false;
+let output = document.getElementById('output');
+
+let btnVerification = document.querySelector('button');
+
+btnVerification.addEventListener('click', function () {
+    for (let i = 0; i < listEmail.length; i++) {
+        const element = listEmail[i];
+        if (element == insertName.value) {
+            output.innerHTML = 'Sei uno degli invitai alla festa';
+            userVerified = true
+        };
+    };
+    if (!userVerified) {
+        output.innerHTML = 'Mi dispiace, non sei uno degli invitati alla festa';
+    }
+})
